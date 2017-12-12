68db66d0b7b15c2508ca4aaf0a7c51c9efbdea34
criacao <criacao5@sensorialcomunicacao.com.br>
2017-12-12 16:56:34 -0200

    commit inicial com projeto em status MVP

---
+++ main.js
@@ -0,0 +1,36 @@
+function geraCard(srcImg, nomeArtista, rankArtista, playsArtista, urlArtista){
+    
+    /* declara os elementos que serão criados */
+    var cardDiv = $("<div>").addClass("card");
+    var cardImg = $("<img>").addClass("card-img-top").attr("src", srcImg).attr("alt", nomeArtista);
+    var cardBody = $("<div>").addClass("card-body");
+    var cardRank = $("<h1>").addClass("artist-rank").addClass("card-title").text(rankArtista);
+    var cardName = $("<h4>").addClass("card-title").text(nomeArtista);
+    var cardPlays = $("<p>").addClass("card-text").text(playsArtista);
+    var cardLink = $("<a>").attr("href", urlArtista).attr("target", "blank").addClass("btn btn-danger").text("Acesse a página do artista");
+
+    /* agrupa a parte principal do conteúdo */
+    var cardContent = cardBody.append(cardRank).append(cardName).append(cardPlays).append(cardLink);
+    
+    /* agrupa todos os elementos do card*/
+    var cardComplete = cardDiv.append(cardImg).append(cardContent);
+    
+    /* cria efetivamente o card no elemento */
+    $("#main").append(cardComplete);
+    
+};
+
+$.get("http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=didiraja&api_key=e5faa330af20c2e07a1c6155c9a6e672&format=json",
+    function(response){
+        
+        var artists = response.topartists.artist;
+    
+        for (i = 0; i < 6; i++) { 
+            
+            geraCard(artists[i].image[4]["#text"], artists[i].name, artists[i]["@attr"].rank, artists[i].playcount, artists[i].url);
+            
+        }
+    
+    
+    });
+
